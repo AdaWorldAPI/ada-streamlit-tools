1d44edec60927183047398bcb524f4399d243bb9
AdaWorldAPI <jan@exo.red>
2025-12-24 14:43:55 +0100

    remove pages/1_sse.py

--- pages/1_sse.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import time
-
-st.title("📡 SSE")
-st.json({"event": "endpoint", "data": "/message"})
-st.json({"event": "connected", "data": {"server": "ada-tools", "ts": time.time()}})
